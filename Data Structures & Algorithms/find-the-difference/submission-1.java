bceab48a8f2c3951550857a15db83259a9df87e9
Hemanth Sembilli <hemanth.sembilli@gmail.com>
2026-09-10 23:00:20 +0530

    Add: find-the-difference - submission-1

---
+++ Data Structures & Algorithms/find-the-difference/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        char res = 0;
+        for (int i = 0;i<n;i++){
+            res ^= s.charAt(i);
+        }
+        for (int i = 0;i<m;i++){
+            res ^= t.charAt(i);
+        }
+        return res;
+    }
+}
