b0319518288c633d686d44faa7186193612975cb
Hemanth Sembilli <hemanth.sembilli@gmail.com>
2026-09-20 14:35:15 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        for (int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for (Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if (entry.getValue()>1){
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
